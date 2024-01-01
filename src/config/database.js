76a9f1102c3afb4ec49760f6fb415cb05926be06
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-01 15:47:38 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: '192.168.0.13',
+    dialect: 'mysql',
+    username: 'root',
+    password: 'pretinho02',
+    database: 'circle',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
