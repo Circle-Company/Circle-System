93620dacf3456e42ae0defbf807f095bf0062805
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-06 19:36:27 -0300

    database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
-    host: '192.168.0.13',
-    dialect: 'mysql',
-    username: 'root',
-    password: 'pretinho02',
-    database: 'circle',
-    define: {
-        timestamps: true,
-        underscored: true
-    }
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  define: {
+      timestamps: true,
+      underscored: true
+  }
 }
