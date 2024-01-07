682ac20bfcf1413c3895d3c4d1f7a066823d70ca
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-07 20:07:05 -0300

    remove cors and add admin router

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import 'express-async-errors'
 import bodyParser from 'body-parser'
 import './database/index.js'
 import config from './config/index.js'
-import cors from 'cors'
 
 const authRouter = require('./routes/auth-router.js')
 const userRouter = require('./routes/user-router.js')
+const adminRouter = require('./routes/admin-router.js')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(authRouter, userRouter)
+app.use(authRouter, userRouter, adminRouter)
 
 app.listen(config.PORT, () => console.log("🚀 Server running on port: " + config.PORT))
