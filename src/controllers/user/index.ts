2d55c9277c7e489a84d8148bc387cd622a26b210
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-08 11:54:34 -0300

    add block actions

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -2,15 +2,14 @@ import {
     find_user_by_username,
     find_user_data
 } from "./user-find-controller"
-
 import {
-    edit_user_description,
-    delete_user_description
-} from "./user-edit-controller"
+    block_user,
+    unlock_user
+} from "./user-actions-controller"
 
 export const UserController = {
     FindUserByUsername: find_user_by_username,
     FindUserData: find_user_data,
-    EdituserDescription: edit_user_description,
-    DeleteUserDescription: delete_user_description
+    BlockUser: block_user,
+    UnlockUser: unlock_user
 }
