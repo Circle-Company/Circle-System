2b8e9476f3b5d99947c6dd78bafc0f122116a5f8
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-08 11:53:32 -0300

    modify to allow space key

--- src/helpers/contain-special-characters.ts
+++ src/helpers/contain-special-characters.ts
@@ -1,14 +1,26 @@
 type ContainSpecialCharactersProps = {
-    text: string
+    text: string,
+    allow_space_point?: boolean
 }
 
 export async function ContainSpecialCharacters({
-    text
+    text,
+    allow_space_point = true,
 }: ContainSpecialCharactersProps): Promise<boolean> {
-    const regex = /^[a-zA-Z0-9_.]+$/
-    if (!regex.test(text)) {
-      const specialCharacters = text.split('').filter(char => !regex.test(char))
-      return specialCharacters.length > 0 ? true : false
+    const regex_allow_space_point = /^[a-zA-Z0-9_.]+$/
+    const regex = /^[a-zA-Z\s]+$/
+
+    if(allow_space_point){
+        if (!regex_allow_space_point.test(text)) {
+            const specialCharacters = text.split('').filter(char => !regex.test(char))
+            return specialCharacters.length > 0 ? true : false
+        }
+        return false       
+    }else {
+        if (!regex.test(text)) {
+            const specialCharacters = text.split('').filter(char => !regex.test(char))
+            return specialCharacters.length > 0 ? true : false
+        }
+        return false   
     }
-    return false
 }
