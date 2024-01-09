070a289c5cf302e6b9f11eebfd168d3d7c1a9177
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-09 14:34:42 -0300

    add routes

--- src/routes/account-router.ts
+++ src/routes/account-router.ts
@@ -12,7 +12,10 @@ const ACC_DELETE_PREFIX = ACC_PREFIX + RP.DELETE
 accountRouter.put(ACC_EDIT_PREFIX + '/description', AccountController.EdituserDescription)
 accountRouter.put(ACC_EDIT_PREFIX + '/name', AccountController.EditUserName)
 accountRouter.put(ACC_EDIT_PREFIX + '/profile-picture', AccountController.EditProfilePicture)
+accountRouter.put(ACC_EDIT_PREFIX + '/username', AccountController.EditUserUsername)
+accountRouter.put(ACC_EDIT_PREFIX + '/coordinates', AccountController.EditCoordinates)
 
 accountRouter.delete(ACC_DELETE_PREFIX + '/description', AccountController.DeleteUserDescription)
+accountRouter.delete(ACC_DELETE_PREFIX + '/name', AccountController.DeleteUserName)
 accountRouter.delete(ACC_DELETE_PREFIX + '/profile-picture', AccountController.DeleteProfilePicture)
 module.exports = accountRouter
