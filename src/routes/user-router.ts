40d8522685564be84265b418297ce883b0340c4d
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-09 14:34:21 -0300

    add search and follow routes

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -10,7 +10,11 @@ const USER_PROFILE_PREFIX = USER_PREFIX + RP.PROFILE
 userRouter.get(USER_PROFILE_PREFIX + '/:username', UserController.FindUserByUsername)
 userRouter.get(USER_PROFILE_PREFIX + '/data/:username', UserController.FindUserData)
 
+userRouter.post(USER_PREFIX + '/search', UserController.SearchUser)
+
 userRouter.post(USER_PREFIX + '/block', UserController.BlockUser)
 userRouter.post(USER_PREFIX + '/unlock', UserController.UnlockUser)
+userRouter.post(USER_PREFIX + '/follow', UserController.FollowUser)
+userRouter.post(USER_PREFIX + '/unfollow', UserController.UnfollowUser)
 
 module.exports = userRouter
