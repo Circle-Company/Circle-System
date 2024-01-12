0938367955143ae432404ffd043a7e5cc6a12492
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-12 01:23:00 -0300

    add comments

--- src/search_engine/src/modules/apply_candidates_weights.ts
+++ src/search_engine/src/modules/apply_candidates_weights.ts
@@ -5,9 +5,11 @@ export async function apply_candidates_weights({
     candidates
 }: ApplyCandidatesWeights) {
     const candidates_with_weights = candidates.map((candidate: any) =>{
-        // Calcula a pontuação total para o candidato usando a função de scoringUtils
+
+        // Calculates the total_score for the candidate using the scoringUtils function
         const totalScore = ScoringUtils.calculateTotalScore(candidate)
-        // Retorna um objeto contendo as informações relevantes usando a função de scoringUtils
+
+        // returns the candidate and their total_score
         return ScoringUtils.generateResultObject(candidate, totalScore)
     })
 
