d7b9022e062fc9ee561e10794a8ace0adfb2d350
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-19 20:09:32 -0300

    refactor auto_add_function

--- src/helpers/relation/auto-add-relation.ts
+++ src/helpers/relation/auto-add-relation.ts
@@ -7,12 +7,9 @@ const Relation = require('../../models/user/relation-model.js')
 export async function auto_add_relation({
     user_id, related_user_id, weight
 }: AutoAddRelationProps) {
-    const relation_exists = await find_relation_exists({
-        user_id, related_user_id 
-    })
-    console.log(relation_exists)
+    const relation_exists = await find_relation_exists({user_id, related_user_id})
     if(relation_exists){
-
+        await edit_relation({user_id, related_user_id, increment_weight: weight})
     } else {
         await Relation.create({ user_id, related_user_id, weight })
     }
