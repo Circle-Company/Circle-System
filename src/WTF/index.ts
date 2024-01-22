d2fd795690a7becb13584c2e774b12061df11cce
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-22 18:10:48 -0300

    add WTF ( who to follow )

--- src/WTF/index.ts
+++ src/WTF/index.ts
@@ -1,17 +1,17 @@
 import { findInitCandidates } from "./modules/generate_user_follows_graph/find_init_candidates";
 import { generateUserFollowsGraph } from "./modules/generate_user_follows_graph";
-type UserRecommenderProps = {
+type WTFProps = {
     user_id: number;
 };
 
 /**
  * Main user recommender engine function that orchestrates the user recommendation process.
- * @param {UserRecommenderProps} props - Input properties containing user id
+ * @param {WTFProps} props - Input properties containing user id
  * @returns {Promise<any>} - A Promise that resolves to the sorted recommender candidates.
  */
-export async function UserRecommenderEngine({
+export async function WTF({
     user_id, 
-}: UserRecommenderProps): Promise<any> {
+}: WTFProps): Promise<any> {
 
     const init_candidates = await generateUserFollowsGraph({ user_id})
 
