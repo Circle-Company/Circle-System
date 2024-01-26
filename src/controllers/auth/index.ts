e6facb27a3a756562b9d83adf55c81f71af7c0a3
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-26 17:00:21 -0300

    add send_socket

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,10 +1,11 @@
 import { authenticate_user, refresh_token} from "./authenticate-auth-controller";
-import { store_new_user, verify_code, send_verification_code} from "./store-auth-controller";
+import { store_new_user, verify_code, send_verification_code, send_socket} from "./store-auth-controller";
 
 export const AuthController = {
     StoreNewUser: store_new_user,
     AuthenticateUser: authenticate_user,
     SendVerificationCode: send_verification_code,
     VerifyCode: verify_code,
-    RefreshToken: refresh_token
+    RefreshToken: refresh_token,
+    SendSocket: send_socket
 }
