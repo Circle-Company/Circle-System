881627b455562db6c324707c08745649c46ea444
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-01-26 17:00:02 -0300

    add moments migration

---
+++ src/database/migrations/20240121053029-create-moments.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('moments', {
+      id: {
+        type: Sequelize.INTEGER(),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER(),
+        allowNull: false,
+        references: {model: 'users', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'  
+      }, 
+      description: {
+        type: Sequelize.STRING(300),
+        defaultValue: null
+      },
+      deleted: {
+        type: Sequelize.BOOLEAN(),
+        defaultValue: false,
+        allowNull: false
+      },
+      blocked: {
+        type: Sequelize.BOOLEAN(),
+        defaultValue: false,
+        allowNull: false
+      },    
+      created_at: {
+        type: Sequelize.DATE(),
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE(),
+        allowNull: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('moments')
+  }
+};
