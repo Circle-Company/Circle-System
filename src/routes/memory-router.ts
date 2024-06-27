0e1105c7232193b445138dea407f6a5cc89d158a
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-27 18:50:15 -0300

    add: memory router

--- src/routes/memory-router.ts
+++ src/routes/memory-router.ts
@@ -9,10 +9,13 @@ const MEMORY_PREFIX = RP.API_VERISON + RP.MEMORY
 memoryRouter.post(MEMORY_PREFIX + '/get', MemoryController.FindMemory)
 memoryRouter.post(MEMORY_PREFIX + '/get-user-memories', MemoryController.FindUserMemories)
 memoryRouter.post(MEMORY_PREFIX + '/get-moments', MemoryController.FindMemoryMoments)
+memoryRouter.post(MEMORY_PREFIX + '/get-moments-ids', MemoryController.FindMemoryMomentsIds)
+
+memoryRouter.post(MEMORY_PREFIX + '/edit/title', MemoryController.EditMemoryTitle)
 
 memoryRouter.post(MEMORY_PREFIX + '/create', MemoryController.StoreNewMemory)
 memoryRouter.post(MEMORY_PREFIX + '/add-moment', MemoryController.StoreNewMemoryMoment)
 
-memoryRouter.delete(MEMORY_PREFIX + '/delete', MemoryController.DeleteMemory)
-memoryRouter.delete(MEMORY_PREFIX + '/remove-moment', MemoryController.DeleteMemoryMoment)
+memoryRouter.post(MEMORY_PREFIX + '/delete', MemoryController.DeleteMemory)
+memoryRouter.post(MEMORY_PREFIX + '/remove-moment', MemoryController.DeleteMemoryMoment)
 module.exports = memoryRouter
