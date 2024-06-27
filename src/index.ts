7f4be1cb6b1f70736fa6360e21dc4cd8480447c6
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-27 18:51:58 -0300

    fix: index configurations

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,33 @@ import express from 'express'
 import http from 'http'
 import 'express-async-errors'
 import bodyParser from 'body-parser'
-import './database/index.js'
+import './database/index'
 import config from './config/index.js'
 import {initializeSocket, getSocketInstance} from  './config/socket.js'
 
-const authRouter = require('./routes/auth-router.js')
-const userRouter = require('./routes/user-router.js')
-const adminRouter = require('./routes/admin-router.js')
-const accountRouter = require('./routes/account-router.js')
-const moderatorRouter = require('./routes/moderator-router.js')
-const NotificationRouter = require('./routes/notification-router.js')
+const authRouter = require('./routes/auth-router')
+const userRouter = require('./routes/user-router')
+const momentRouter = require('./routes/moment-router')
+const memoryRouter = require('./routes/memory-router')
+const adminRouter = require('./routes/admin-router')
+const accountRouter = require('./routes/account-router')
+const moderatorRouter = require('./routes/moderator-router')
+const NotificationRouter = require('./routes/notification-router')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(authRouter, userRouter, accountRouter, NotificationRouter)
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(authRouter, userRouter, accountRouter, NotificationRouter, momentRouter, memoryRouter)
 app.use(adminRouter, moderatorRouter)
 
+/**
+ * 
 const server = http.createServer();
 initializeSocket({server});
-getSocketInstance().listen(3001)
+getSocketInstance().listen(config.SOCKET_PORT)
 
-app.listen(config.PORT, () => console.log("🚀 Server running on port: " + config.PORT))
+ */
+
+
+app.listen(config.PORT, () => console.log("🚀 circle-system (server) - running on port: " + config.PORT))
