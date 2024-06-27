1248224ca452b8ea1df5fc2efd53f7390f4c29b9
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-27 18:50:05 -0300

    add: auth router

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -6,11 +6,13 @@ const authRouter = Router()
 
 const AUTH_PREFIX = RP.API_VERISON + RP.AUTH
 
-authRouter.post( AUTH_PREFIX + '/signup', AuthController.StoreNewUser)
-authRouter.post( AUTH_PREFIX + '/signin', AuthController.AuthenticateUser)
+authRouter.post( AUTH_PREFIX + '/sign-up', AuthController.StoreNewUser)
+authRouter.post( AUTH_PREFIX + '/sign-in', AuthController.AuthenticateUser)
 authRouter.post( AUTH_PREFIX + '/send-verification-code', AuthController.SendVerificationCode)
 authRouter.post( AUTH_PREFIX + '/verify-code', AuthController.VerifyCode)
 authRouter.post( AUTH_PREFIX + '/refresh-token', AuthController.RefreshToken)
 authRouter.post( AUTH_PREFIX + '/send-socket', AuthController.SendSocket)
+authRouter.post( AUTH_PREFIX + '/username-already-in-use', AuthController.UsernameAlreadyInUse)
+authRouter.put( AUTH_PREFIX + '/change-password', AuthController.ChangePassword)
 
 module.exports = authRouter
