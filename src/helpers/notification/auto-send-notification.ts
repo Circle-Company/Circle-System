6165279050b1c5b8ddb9a805b15193a42075372c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-06-27 18:46:01 -0300

    add: notification helper

--- src/helpers/notification/auto-send-notification.ts
+++ src/helpers/notification/auto-send-notification.ts
@@ -3,11 +3,15 @@ import { send_notification } from "./send-notification"
 import { AutoSendNotificationProps } from "./types"
 import { InternalServerError } from "../../errors"
 
-
 export async function auto_send_notification(props: AutoSendNotificationProps) {
     try{
-        const notification = await create_notification(props)
-        await send_notification({notification: notification})        
+        await create_notification({
+            receiver_user_id: props.receiver_user_id,
+            sender_user_id: props.sender_user_id,
+            type: props.type,
+            content_id: props.content_id? props.content_id : null
+        })
+        //await send_notification({notification: notification})        
     } catch{
         return new InternalServerError({
             message: 'We are unable to create and send notifications',
--- src/helpers/notification/send-notification.ts
+++ src/helpers/notification/send-notification.ts
@@ -1,10 +1,12 @@
 import { SendNotificationProps } from "./types"
 import  { getSocketInstance } from '../../config/socket'
-const Socket = require('../../models/user/socket-model.js')
-const Notification = require('../../models/user/notification-model.js')
-const ProfilePicture = require('../../models/user/profilepicture-model.js')
-const User = require('../../models/user/user-model.js')
-const Follow = require('../../models/user/follow-model.js')
+import Socket from '../../models/user/socket-model.js';
+import Notification from '../../models/user/notification-model.js';
+import ProfilePicture from '../../models/user/profilepicture-model.js';
+import User from '../../models/user/user-model.js';
+import Follow from '../../models/user/follow-model.js';
+import MomentMidia from '../../models/moments/moment_midia-model.js'
+
 
 export async function send_notification({
     notification
@@ -28,20 +30,34 @@ export async function send_notification({
         attributes: ['followed_user_id', 'user_id'],
         where: { followed_user_id: user.id, user_id: notification.receiver_user_id}
     })
+
+    let momentMidia: {nhd_resolution: string} | null = null
+
+    if(
+        notification.type =='LIKE-MOMENT'
+        || notification.type =='LIKE-MOMENT-2'
+        || notification.type == 'LIKE-MOMENT-3'
+        && notification.content_id !== null
+    ) {
+        momentMidia = await MomentMidia.findOne({
+            attributes: ['nhd_resolution'],
+            where: { moment_id: notification.content_id}
+        })
+    } else momentMidia = null
     io.to(socket.socket_id.toString()).emit(`new-notification`, {
         id: notification.id,
         receiver_user_id: notification.receiver_user_id,
         viewed: notification.viewed,
         type: notification.type,
-        created_at: notification.created_at,
+        created_at: notification.created_at,    
+        midia: momentMidia,
         sender_user: {
             id: user.id,
             username: user.username,
             verifyed: user.verifyed,
-            profile_pictures: user.profile_pictures,
-            you_follow:Boolean(user_followed)
-
-        }
+            profile_picture: user.profile_pictures,
+        },
+        you_follow:Boolean(user_followed)
     })
     console.log(io)
 
