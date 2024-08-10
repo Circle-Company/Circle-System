0b728c472ace433c31f7dfcb720f9862b273216c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-10 13:02:42 -0300

    add: modify notification model + migration

--- src/database/migrations/20240123171902-create-notifications.js
+++ src/database/migrations/20240123171902-create-notifications.js
@@ -1,54 +1,70 @@
-'use strict';
+"use strict"
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.createTable('notifications', {
-      id: {
-        type: Sequelize.INTEGER(),
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      sender_user_id: {
-        type: Sequelize.INTEGER(),
-        allowNull: false,
-        references: {model: 'users', key: 'id'},
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'  
-      },
-      receiver_user_id: {
-        type: Sequelize.INTEGER(),
-        allowNull: false,
-        references: {model: 'users', key: 'id'},
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'  
-      },
-      viewed: {
-        type: Sequelize.BOOLEAN(),
-        defaultValue: false,
-        allowNull: false
-      },  
-      type: {
-        type: Sequelize.STRING(),
-        allowNull: false
-      },
-      content_id: {
-        type: Sequelize.INTEGER(),
-        allowNull: true,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
-    })
-  },
+    async up(queryInterface, Sequelize) {
+        return queryInterface.createTable("notifications", {
+            id: {
+                type: Sequelize.INTEGER(),
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            sender_user_id: {
+                type: Sequelize.INTEGER(),
+                allowNull: false,
+                references: { model: "users", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            receiver_user_id: {
+                type: Sequelize.INTEGER(),
+                allowNull: true,
+                references: { model: "users", key: "id" },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            moment_id: {
+                type: Sequelize.INTEGER(),
+                allowNull: true,
+                references: {
+                    model: "moments", // Certifique-se de ajustar para o nome real da tabela de moments
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            memory_id: {
+                type: Sequelize.INTEGER(),
+                allowNull: true,
+                references: {
+                    model: "memories", // Certifique-se de ajustar para o nome real da tabela de memories
+                    key: "id",
+                },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE",
+            },
+            viewed: {
+                type: Sequelize.BOOLEAN(),
+                defaultValue: false,
+                allowNull: false,
+            },
+            type: {
+                type: Sequelize.STRING(),
+                allowNull: false,
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        })
+    },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.dropTable('notifications')
-  }
-};
+    async down(queryInterface, Sequelize) {
+        return queryInterface.dropTable("notifications")
+    },
+}
--- src/models/memories/memory-model.js
+++ src/models/memories/memory-model.js
@@ -1,23 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize")
 
 class Memory extends Model {
     static init(sequelize) {
-        super.init({
-            user_id: DataTypes.INTEGER(),
-            title: DataTypes.STRING(),
-            // Adicione outros campos relevantes para as memories
-        }, {
-            sequelize,
-            modelName: 'Memory',
-            tableName: 'memories'
-        });
+        super.init(
+            {
+                user_id: DataTypes.INTEGER(),
+                title: DataTypes.STRING(),
+                // Adicione outros campos relevantes para as memories
+            },
+            {
+                sequelize,
+                modelName: "Memory",
+                tableName: "memories",
+            }
+        )
     }
 
     static associate(models) {
-        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
-        this.hasMany(models.MemoryMoment, { foreignKey: 'memory_id', as: 'memoryMoments' }); // Alterado para hasMany
-        this.belongsToMany(models.Moment, { through: 'MemoryMoments', foreignKey: 'memory_id', as: 'moments' });
+        this.belongsTo(models.User, { foreignKey: "user_id", as: "user" })
+        this.hasMany(models.MemoryMoment, { foreignKey: "memory_id", as: "memoryMoments" }) // Alterado para hasMany
+        this.belongsToMany(models.Moment, {
+            through: "MemoryMoments",
+            foreignKey: "memory_id",
+            as: "moments",
+        })
+        this.hasMany(models.Notification, { foreignKey: "memory_id" })
     }
 }
 
-module.exports = Memory;
+module.exports = Memory
