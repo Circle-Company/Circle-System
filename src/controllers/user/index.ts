143b0d9ccd806fa516971202b3ffb0b42ae52ae7
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-13 21:50:53 -0300

    refactor: eslint refactor

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,20 +1,21 @@
 import {
+    block_user,
+    follow_user,
+    report,
+    unfollow_user,
+    unlock_user,
+    updateUserCoordinates,
+} from "./user-actions-controller"
+import {
+    find_most_followed_users,
+    find_session_user_by_pk,
+    find_session_user_statistics_by_pk,
+    find_user_by_pk,
     find_user_by_username,
     find_user_data,
-    search_user,
     recommender_users,
-    find_most_followed_users,
-    find_user_by_pk,
-    find_session_user_by_pk,
-    find_session_user_statistics_by_pk
+    search_user,
 } from "./user-find-controller"
-import {
-    block_user,
-    unlock_user,
-    follow_user,
-    unfollow_user,
-    report
-} from "./user-actions-controller"
 
 export const UserController = {
     SearchUser: search_user,
@@ -30,5 +31,5 @@ export const UserController = {
     UnlockUser: unlock_user,
     FollowUser: follow_user,
     UnfollowUser: unfollow_user,
-    Report: report
+    Report: report,
 }
