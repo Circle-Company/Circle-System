e831d57fb688b7ec8fba0b617f6353bb9488c1ef
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-20 19:50:35 -0300

    add: preferences router + eslint refactor

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
-import express from 'express'
-import http from 'http'
-import 'express-async-errors'
-import bodyParser from 'body-parser'
-import './database/index'
-import config from './config/index.js'
-import {initializeSocket, getSocketInstance} from  './config/socket.js'
+import bodyParser from "body-parser"
+import express from "express"
+import "express-async-errors"
+import config from "./config/index.js"
+import "./database/index"
 
-const authRouter = require('./routes/auth-router')
-const userRouter = require('./routes/user-router')
-const momentRouter = require('./routes/moment-router')
-const memoryRouter = require('./routes/memory-router')
-const adminRouter = require('./routes/admin-router')
-const accountRouter = require('./routes/account-router')
-const moderatorRouter = require('./routes/moderator-router')
-const NotificationRouter = require('./routes/notification-router')
+const authRouter = require("./routes/auth-router")
+const userRouter = require("./routes/user-router")
+const momentRouter = require("./routes/moment-router")
+const memoryRouter = require("./routes/memory-router")
+const adminRouter = require("./routes/admin-router")
+const accountRouter = require("./routes/account-router")
+const moderatorRouter = require("./routes/moderator-router")
+const NotificationRouter = require("./routes/notification-router")
+const PreferencesRouter = require("./routes/preferences-router")
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(authRouter, userRouter, accountRouter, NotificationRouter, momentRouter, memoryRouter)
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(
+    authRouter,
+    userRouter,
+    accountRouter,
+    NotificationRouter,
+    momentRouter,
+    memoryRouter,
+    PreferencesRouter
+)
 app.use(adminRouter, moderatorRouter)
 
 /**
@@ -30,5 +37,6 @@ getSocketInstance().listen(config.SOCKET_PORT)
 
  */
 
-
-app.listen(config.PORT, () => console.log("🚀 circle-system (server) - running on port: " + config.PORT))
+app.listen(config.PORT, () =>
+    console.log("🚀 circle-system (server) - running on port: " + config.PORT)
+)
