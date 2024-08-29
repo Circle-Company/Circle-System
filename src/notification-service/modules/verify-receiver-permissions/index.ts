2e7de6e658e6ecd510c82a8746272a2341e6545c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-28 22:00:45 -0300

    add(notification-service): filter receiver users by preferences

---
+++ src/notification-service/modules/verify-receiver-permissions/index.ts
@@ -0,0 +1,70 @@
+import Preference from "../../../models/preferences/preference-model.js"
+import { NotificationProps } from "../../types.js"
+
+type User = {
+    id: number
+    token: string
+}
+
+type ModuleProps = {
+    usersList: User[]
+    notification: NotificationProps
+}
+
+export async function Module({ usersList, notification }: ModuleProps): Promise<(User | null | undefined)[]> {
+    const results = await Promise.all(
+        usersList.map(async (user) => {
+            const userPreferences = await Preference.findOne({
+                where: { user_id: user.id },
+                attributes: [
+                    "disable_like_moment_push_notification",
+                    "disable_new_memory_push_notification",
+                    "disable_add_to_memory_push_notification",
+                    "disable_follow_user_push_notification",
+                    "disable_view_user_push_notification",
+                    "disable_news_push_notification",
+                    "disable_sugestions_push_notification",
+                    "disable_around_you_push_notification",
+                ],
+            })
+
+            if (!userPreferences) {
+                throw new Error(`Preferences not found for user ID: ${user.id}`)
+            }
+
+            const userPreferencesObj = {
+                disableLikeMoment: userPreferences.disable_like_moment_push_notification,
+                disableNewMemory: userPreferences.disable_new_memory_push_notification,
+                disableAddToMemory: userPreferences.disable_add_to_memory_push_notification,
+                disableFollowUser: userPreferences.disable_follow_user_push_notification,
+                disableViewUser: userPreferences.disable_view_user_push_notification,
+                disableNews: userPreferences.disable_news_push_notification,
+                disableSugestions: userPreferences.disable_sugestions_push_notification,
+                disableAroundYou: userPreferences.disable_around_you_push_notification,
+            }
+
+            if (notification.type == "LIKE-MOMENT") {
+                if (userPreferencesObj.disableLikeMoment == false) return user
+                else return null
+            }
+            if (notification.type == "NEW-MEMORY") {
+                if (userPreferencesObj.disableNewMemory == false) return user
+                else return null
+            }
+            if (notification.type == "ADD-TO-MEMORY") {
+                if (userPreferencesObj.disableAddToMemory == false) return user
+                else return null
+            }
+            if (notification.type == "FOLLOW-USER") {
+                if (userPreferencesObj.disableFollowUser == false) return user
+                else return null
+            }
+            if (notification.type == "VIEW-USER") {
+                if (userPreferencesObj.disableViewUser == false) return user
+                else return null
+            }
+        })
+    )
+
+    return results.filter((value) => value !== null)
+}
