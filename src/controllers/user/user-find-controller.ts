a3b383dd03c19c154827be7c4a5334bc2e682dca
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-28 22:56:52 -0300

    refactor: eslint refactor

--- src/controllers/user/user-find-controller.ts
+++ src/controllers/user/user-find-controller.ts
@@ -1,66 +1,70 @@
-import {Request, Response } from 'express'
-import { UserService } from '../../services/user-service'
-import { MFU } from '../../MFU'
+import { Request, Response } from "express"
+import { MFU } from "../../MFU"
+import { UserService } from "../../services/user-service"
 
-export async function find_user_by_username (req: Request, res: Response) {
-    const { username }  = req.params
+export async function find_user_by_username(req: Request, res: Response) {
+    const { username } = req.params
     const { user_id } = req.body
-    const user = await UserService.UserFind.FindByUsername({username, user_id})
-    return res.status(200).json(user)
+    try {
+        const user = await UserService.UserFind.FindByUsername({ username, user_id })
+        return res.status(200).json(user)
+    } catch (err: any) {
+        console.log(err)
+    }
 }
 
-export async function find_user_by_pk (req: Request, res: Response) {
-    const { user_pk }  = req.params
-    const { user_id } = req.body 
-    const user = await UserService.UserFind.FindByPk({user_id, user_pk})
-    return res.status(200).json(user)
-}
-
-export async function find_session_user_by_pk (req: Request, res: Response) {
-    const { user_pk }  = req.params
-    const user = await UserService.UserFind.FindSessionByPk({user_pk})
-    return res.status(200).json(user)
-}
-
-export async function find_session_user_statistics_by_pk (req: Request, res: Response) {
-    const { user_pk }  = req.params
-    const user = await UserService.UserFind.FindSessionStatisticsByPk({user_pk})
-    return res.status(200).json(user)
-}
-
-export async function find_user_data (req: Request, res: Response) {
-    const { username }  = req.params
+export async function find_user_by_pk(req: Request, res: Response) {
+    const { user_pk } = req.params
     const { user_id } = req.body
-    const user = await UserService.UserFind.FindAllData({username, user_id})
+    const user = await UserService.UserFind.FindByPk({ user_id, user_pk })
     return res.status(200).json(user)
 }
 
-export async function search_user (req: Request, res: Response) {
+export async function find_session_user_by_pk(req: Request, res: Response) {
+    const { user_pk } = req.params
+    const user = await UserService.UserFind.FindSessionByPk({ user_pk })
+    return res.status(200).json(user)
+}
+
+export async function find_session_user_statistics_by_pk(req: Request, res: Response) {
+    const { user_pk } = req.params
+    const user = await UserService.UserFind.FindSessionStatisticsByPk({ user_pk })
+    return res.status(200).json(user)
+}
+
+export async function find_user_data(req: Request, res: Response) {
+    const { username } = req.params
+    const { user_id } = req.body
+    const user = await UserService.UserFind.FindAllData({ username, user_id })
+    return res.status(200).json(user)
+}
+
+export async function search_user(req: Request, res: Response) {
     const { username_to_search, user_id } = req.body
 
     const search_result = await UserService.UserFind.SearchUser({
         user_id,
-        username_to_search
+        username_to_search,
     })
     res.status(200).json(search_result)
 }
 
-export async function recommender_users (req: Request, res: Response) {
+export async function recommender_users(req: Request, res: Response) {
     const { user_id } = req.body
 
-    const recommendations = await UserService.UserFind.RecommenderUsers({user_id})
+    const recommendations = await UserService.UserFind.RecommenderUsers({ user_id })
     res.status(200).json(recommendations)
 }
 
 export async function find_most_followed_users(req: Request, res: Response) {
     try {
-        const page = parseInt(req.query.page as string, 10) || 1;
-        const pageSize = parseInt(req.query.pageSize as string, 10) || 10;
-        const ranking = await MFU({page, pageSize})
+        const page = parseInt(req.query.page as string, 10) || 1
+        const pageSize = parseInt(req.query.pageSize as string, 10) || 10
+        const ranking = await MFU({ page, pageSize })
 
         res.status(200).json(ranking)
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Erro ao obter os usuários mais seguidos.' });
+        console.error(error)
+        res.status(500).json({ error: "Erro ao obter os usuários mais seguidos." })
     }
 }
