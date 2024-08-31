2b20329e6574c88d173947898c302f26a994c7e4
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-08-30 22:36:24 -0300

    refactor: eslint refactor

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { /* ts-jest config goes here in Jest */ }],
-  },
-};
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testMatch: ["**/**/*.test.ts", "**/**/*.spec.ts"],
+    moduleFileExtensions: ["ts", "js", "json", "node"],
+    transform: {
+        "^.+\\.tsx?$": ["ts-jest", "babel-jest"],
+    },
+}
