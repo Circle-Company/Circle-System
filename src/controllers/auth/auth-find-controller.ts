cd93ba276070229fbf64d9a931c034bb69a14b95
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-03 01:44:47 -0300

    refactor: eslint refactor

--- src/controllers/auth/auth-find-controller.ts
+++ src/controllers/auth/auth-find-controller.ts
@@ -1,15 +1,14 @@
-import {Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-import { InternalServerError } from '../../errors'
-import { AuthService } from '../../services/auth-service'
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { InternalServerError } from "../../errors"
+import { AuthService } from "../../services/auth-service"
 
-export async function find_username_already_in_use (req: Request, res: Response) {
+export async function find_username_already_in_use(req: Request, res: Response) {
     const { username } = req.body
     try {
-       const result = await AuthService.Find.UsernameAlreadyInUse({username})
-       res.status(StatusCodes.ACCEPTED).json(result)
-    } catch(err: any) {
-        throw new InternalServerError({message: err.message })
+        const result = await AuthService.Find.UsernameAlreadyInUse({ username })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: any) {
+        throw new InternalServerError({ message: err.message })
     }
-    
 }
