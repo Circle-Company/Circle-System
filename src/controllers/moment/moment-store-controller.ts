06e0f7b298489e63ff8e85714f3997d77bc22647
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-03 01:48:30 -0300

    refactor: use req.user_id + eslint refactor

--- src/controllers/moment/moment-store-controller.ts
+++ src/controllers/moment/moment-store-controller.ts
@@ -1,15 +1,26 @@
-import {Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes';
-import { MomentService } from '../../services/moment-service'
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { MomentService } from "../../services/moment-service"
 
-export async function store_new_moment (req: Request, res: Response) {
-    const { user_id, moment } = req.body
-    const result = await MomentService.Store.NewMoment({user_id, moment})
-    res.status(StatusCodes.ACCEPTED).json(result)
+export async function store_new_moment(req: Request, res: Response) {
+    const { moment } = req.body
+    try {
+        const result = await MomentService.Store.NewMoment({ user_id: Number(req.user_id), moment })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.log(err)
+    }
 }
 
-export async function store_moment_interaction(req: Request, res: Response){
-    const { interaction, user_id, moment_id, moment_owner_id} = req.body
-    const result = await MomentService.Store.Interaction({interaction, user_id, moment_id, moment_owner_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+export async function store_moment_interaction(req: Request, res: Response) {
+    try {
+        const result = await MomentService.Store.Interaction({
+            interaction: req.body,
+            user_id: Number(req.user_id),
+            moment_id: Number(req.params.id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.log(err)
+    }
 }
