2cba0ff0c9fd1c7a3b3d38aa9230d0791a87c2c2
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-03 01:49:11 -0300

    refactor: user req.user_id + req.params.moment_id

--- src/controllers/moment/moment-actions-controller.ts
+++ src/controllers/moment/moment-actions-controller.ts
@@ -1,83 +1,187 @@
-import {Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes';
-import { MomentService } from '../../services/moment-service'
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { ValidationError } from "sequelize"
+import { UnauthorizedError } from "../../errors"
+import { MomentService } from "../../services/moment-service"
 
 export async function view_moment(req: Request, res: Response) {
-    const {user_id, moment_id} = req.body
-    const result = await MomentService.Actions.View({user_id, moment_id})
+    const result = await MomentService.Actions.View({
+        user_id: req.user_id,
+        moment_id: req.params.id,
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function like_moment(req: Request, res: Response) {
-    const {user_id, moment_id} = req.body
-    const result = await MomentService.Actions.Like({user_id, moment_id})
+    const result = await MomentService.Actions.Like({
+        user_id: req.user_id,
+        moment_id: req.params.id,
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function unlike_moment(req: Request, res: Response) {
-    const {user_id, moment_id} = req.body
-    const result = await MomentService.Actions.Unlike({user_id, moment_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
-}
-
-export async function comment_on_moment (req: Request, res: Response) {
-    const { user_id, moment_id, content} = req.body
-    const page = parseInt(req.query.page as string, 10) || 1
-    const pageSize = parseInt(req.query.pageSize as string, 10) || 10
-    const result = await MomentService.Actions.CommentOnMoment({
-        user_id, moment_id, content, page, pageSize
+    const result = await MomentService.Actions.Unlike({
+        user_id: req.user_id,
+        moment_id: req.params.id,
     })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
-export async function reply_comment_on_moment (req: Request, res: Response) {
+export async function comment_on_moment(req: Request, res: Response) {
+    const { user_id, moment_id, content } = req.body
+    const page = parseInt(req.query.page as string, 10) || 1
+    const pageSize = parseInt(req.query.pageSize as string, 10) || 10
+    const result = await MomentService.Actions.CommentOnMoment({
+        user_id,
+        moment_id,
+        content,
+        page,
+        pageSize,
+    })
+    res.status(StatusCodes.ACCEPTED).json(result)
+}
+
+export async function reply_comment_on_moment(req: Request, res: Response) {
     const { user_id, moment_id, parent_comment_id, content } = req.body
     const page = parseInt(req.query.page as string, 10) || 1
     const pageSize = parseInt(req.query.pageSize as string, 10) || 10
     const result = await MomentService.Actions.ReplyCommentOnMoment({
-        user_id, moment_id, parent_comment_id, content, page,pageSize
+        user_id,
+        moment_id,
+        parent_comment_id,
+        content,
+        page,
+        pageSize,
     })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
-export async function like_comment (req: Request, res: Response) {
-    const { comment_id } = req.body
-    const result = await MomentService.Actions.LikeComment({comment_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+export async function like_comment(req: Request, res: Response) {
+    try {
+        if (!req.user_id) {
+            throw new UnauthorizedError({
+                message: "User ID is missing. You must be authenticated to access this resource.",
+            })
+        }
+        const result = await MomentService.Actions.LikeComment({
+            comment_id: Number(req.params.id),
+            user_id: Number(req.user_id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error("Error when liking the comment:", err)
+
+        // Verifica o tipo de erro e retorna a resposta apropriada
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ error: err.message })
+        } else if (err instanceof UnauthorizedError) {
+            return res.status(401).json({ error: err.message })
+        } else {
+            // Em caso de erro interno inesperado, retorna um status 500
+            return res.status(500).json({ error: "An unexpected error occurred." })
+        }
+    }
 }
 
-export async function unlike_comment (req: Request, res: Response) {
-    const { comment_id } = req.body
-    const result = await MomentService.Actions.UnlikeComment({comment_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+export async function unlike_comment(req: Request, res: Response) {
+    try {
+        if (!req.user_id) {
+            throw new UnauthorizedError({
+                message: "User ID is missing. You must be authenticated to access this resource.",
+            })
+        }
+        const result = await MomentService.Actions.UnlikeComment({
+            comment_id: Number(req.params.id),
+            user_id: Number(req.user_id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error("Error when unlike comment:", err)
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ error: err.message })
+        } else if (err instanceof UnauthorizedError) {
+            return res.status(401).json({ error: err.message })
+        } else {
+            // Em caso de erro interno inesperado, retorna um status 500
+            return res.status(500).json({ error: "An unexpected error occurred." })
+        }
+    }
 }
 
 export async function hide_moment(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const result = await MomentService.Actions.Hide({moment_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+    try {
+        const result = await MomentService.Actions.Hide({
+            moment_id: Number(req.params.id),
+            user_id: Number(req.user_id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error("Error trying hide moment:", err)
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ error: err.message })
+        } else if (err instanceof UnauthorizedError) {
+            return res.status(401).json({ error: err.message })
+        } else {
+            // Em caso de erro interno inesperado, retorna um status 500
+            return res.status(500).json({ error: "An unexpected error occurred." })
+        }
+    }
 }
 
 export async function unhide_moment(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const result = await MomentService.Actions.Unhide({moment_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+    try {
+        const result = await MomentService.Actions.Unhide({
+            moment_id: Number(req.params.id),
+            user_id: Number(req.user_id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error("Error trying unhide moment:", err)
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ error: err.message })
+        } else if (err instanceof UnauthorizedError) {
+            return res.status(401).json({ error: err.message })
+        } else {
+            // Em caso de erro interno inesperado, retorna um status 500
+            return res.status(500).json({ error: "An unexpected error occurred." })
+        }
+    }
 }
 
 export async function delete_moment(req: Request, res: Response) {
-    const { moment_id, user_id } = req.body
-    const result = await MomentService.Actions.Delete({moment_id, user_id})
-    res.status(StatusCodes.ACCEPTED).json(result)
+    try {
+        const result = await MomentService.Actions.Delete({
+            moment_id: Number(req.params.id),
+            user_id: Number(req.user_id),
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error("Error trying delete moment:", err)
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ error: err.message })
+        } else if (err instanceof UnauthorizedError) {
+            return res.status(401).json({ error: err.message })
+        } else {
+            // Em caso de erro interno inesperado, retorna um status 500
+            return res.status(500).json({ error: "An unexpected error occurred." })
+        }
+    }
 }
 
 export async function delete_moment_list(req: Request, res: Response) {
     const { moment_ids_list } = req.body
-    const result = await MomentService.Actions.DeleteList({moment_ids_list})
+    const result = await MomentService.Actions.DeleteList({
+        moment_ids_list,
+        user_id: Number(req.user_id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function undelete_moment(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const result = await MomentService.Actions.Undelete({moment_id})
+    const result = await MomentService.Actions.Undelete({
+        moment_id: Number(req.params.id),
+        user_id: Number(req.user_id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
--- src/controllers/moment/moment-find-controller.ts
+++ src/controllers/moment/moment-find-controller.ts
@@ -3,7 +3,10 @@ import { StatusCodes } from "http-status-codes"
 import { MomentService } from "../../services/moment-service"
 
 export async function find_user_feed_moments(req: Request, res: Response) {
-    const result = await MomentService.Find.UserFeedMoments({ interaction_queue: req.body })
+    const result = await MomentService.Find.UserFeedMoments({
+        interaction_queue: req.body,
+        user_id: Number(req.user_id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
@@ -33,27 +36,40 @@ export async function find_user_moments_tiny(req: Request, res: Response) {
 }
 
 export async function find_user_moments_tiny_exclude_memory(req: Request, res: Response) {
-    const { user_id, memory_id } = req.body
-    const result = await MomentService.Find.UserMomentsTinyExcludeMemory({ user_id, memory_id })
+    const result = await MomentService.Find.UserMomentsTinyExcludeMemory({
+        memory_id: Number(req.params.id),
+        user_id: Number(req.user_id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function find_moment_comments(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const page = parseInt(req.query.page as string, 10) || 1
-    const pageSize = parseInt(req.query.pageSize as string, 10) || 10
-    const result = await MomentService.Find.MomentComments({ moment_id, pageSize, page })
-    res.status(StatusCodes.ACCEPTED).json(result)
+    try {
+        const page = parseInt(req.query.page as string, 10) || 1
+        const pageSize = parseInt(req.query.pageSize as string, 10) || 10
+        const result = await MomentService.Find.MomentComments({
+            moment_id: req.params.id,
+            user_id: Number(req.user_id),
+            pageSize,
+            page,
+        })
+        res.status(StatusCodes.ACCEPTED).json(result)
+    } catch (err: unknown) {
+        console.error(err)
+    }
 }
 
 export async function find_moment_statistics_view(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const result = await MomentService.Find.MomentStatisticsView({ moment_id })
+    const result = await MomentService.Find.MomentStatisticsView({
+        moment_id: Number(req.params.id),
+        user_id: Number(req.user_id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function find_moment_tags(req: Request, res: Response) {
-    const { moment_id } = req.body
-    const result = await MomentService.Find.MomentTags({ moment_id })
+    const result = await MomentService.Find.MomentTags({
+        moment_id: Number(req.params.id),
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
