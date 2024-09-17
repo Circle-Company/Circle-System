7b5b1d0d5012320b404f5728cdbb2b1b9c6220ce
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-17 01:04:15 -0300

    remove: comment on moment page + pageSize props

--- src/services/moment-service/types.ts
+++ src/services/moment-service/types.ts
@@ -124,8 +124,6 @@ export interface CommentOnMomentProps {
     moment_id: number
     user_id: number
     content: string
-    page: number
-    pageSize: number
 }
 
 export interface ReplyCommentOnMomentProps {
