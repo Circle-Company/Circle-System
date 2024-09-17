c2e7770baaa5c4085046b17526e3ccab93c92964
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-17 00:35:12 -0300

    fix: change post to get method

--- src/routes/notification-router.ts
+++ src/routes/notification-router.ts
@@ -2,5 +2,5 @@ import { Router } from "express"
 import { NotificationController } from "./../controllers/notification"
 
 export const router = Router()
-router.post("/find", NotificationController.FinduserNotifications)
+router.get("/find", NotificationController.FinduserNotifications)
 router.post("/token/store", NotificationController.StoreToken)
