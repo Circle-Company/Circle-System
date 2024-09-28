637d46ee53504fe3a596dbc2d5433528dbcca712
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2024-09-28 11:17:52 -0300

    add: ge4t user id from req.user_id

--- src/controllers/memory/memory-store-controller.ts
+++ src/controllers/memory/memory-store-controller.ts
@@ -1,15 +1,19 @@
-import {Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-import { Memory } from '../../services/memory-service'
+import { Request, Response } from "express"
+import { StatusCodes } from "http-status-codes"
+import { Memory } from "../../services/memory-service"
 
-export async function store_new_memory (req: Request, res: Response) {
+export async function store_new_memory(req: Request, res: Response) {
     const { user_id, title } = req.body
     const result = await Memory.Store.NewMemory({ user_id, title })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
-export async function store_new_memory_moment ( req: Request, res: Response) {
+export async function store_new_memory_moment(req: Request, res: Response) {
     const { memory_id, moments_list } = req.body
-    const result = await Memory.Store.NewMemoryMoment({memory_id, moments_list})
+    const result = await Memory.Store.NewMemoryMoment({
+        user_id: Number(req.user_id),
+        memory_id,
+        moments_list,
+    })
     res.status(StatusCodes.ACCEPTED).json(result)
 }
