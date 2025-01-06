2da9e886560d69b4e209859bb959b06f9511be53
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-06 15:57:46 -0300

    fix: remove findLikedMoments function

--- src/controllers/account/account-find-controller.ts
+++ src/controllers/account/account-find-controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express"
 import { ValidationError } from "../../errors"
-import Like from "../../models/moments/like-model"
 import Follow from "../../models/user/follow-model"
 import ProfilePicture from "../../models/user/profilepicture-model"
 import Statistic from "../../models/user/statistic-model"
@@ -75,56 +74,6 @@ export async function findAccountFollowings(req: Request, res: Response): Promis
 
         if (error instanceof ValidationError) {
             res.status(400).json(error)
-        }
-    }
-}
-
-export async function findLikedMoments(req: Request, res: Response): Promise<void> {
-    try {
-        const { user_id } = req
-
-        if (!user_id) {
-            throw new ValidationError({
-                message: "User ID is required to fetch followings.",
-                action: "Ensure the user is authenticated and their ID is provided.",
-            })
-        }
-
-        // Obtenção de query params para paginação com valores padrão
-        const limit = parseInt(req.query.limit as string, 10) || 10 // Número máximo de registros por página
-        const page = parseInt(req.query.page as string, 10) || 1 // Número da página
-        const offset = (page - 1) * limit
-
-        // Consulta paginada
-        const { rows: likedMoments, count: totalItems } = await Like.findAndCountAll({
-            where: { user_id: user_id.toString() },
-            limit,
-            offset,
-        })
-
-        // Construção do objeto de paginação
-        const totalPages = Math.ceil(totalItems / limit)
-
-        res.status(200).json({
-            data: likedMoments,
-            pagination: {
-                totalItems,
-                totalPages,
-                currentPage: page,
-                pageSize: limit,
-            },
-        })
-    } catch (error) {
-        console.error("Error fetching following users:", error)
-
-        if (error instanceof ValidationError) {
-            res.status(400).json({
-                success: false,
-                error: {
-                    message: error.message,
-                    action: error.action,
-                },
-            })
         }
     }
 }
