7443c99e9e68ae86777f14c29b169be0044fb6db
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-06 15:52:51 -0300

    refactor: change syntax

--- src/apis/swipe-engine/index.ts
+++ src/apis/swipe-engine/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import config from "../../config"
 
-export const swipe_engine_api = axios.create({
+export const swipeEngineApi = axios.create({
     baseURL: config.CIRCLE_SWIPE_ENGINE_API,
 })
