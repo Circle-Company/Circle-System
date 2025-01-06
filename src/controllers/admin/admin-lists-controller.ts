2800764ebd8d0168d5969f995567b17218b82c8f
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-06 15:58:31 -0300

    fix: imports + eslint refactor

--- src/controllers/admin/admin-lists-controller.ts
+++ src/controllers/admin/admin-lists-controller.ts
@@ -1,156 +1,167 @@
-import { Request, Response } from 'express'
+import { Request, Response } from "express"
 import { NotFoundError } from "../../errors"
-import User from '../../models/user/user-model.js';
-import ProfilePicture from '../../models/user/profilepicture-model.js';
+import ProfilePicture from "../../models/user/profilepicture-model"
+import User from "../../models/user/user-model"
 
-
-export async function list_blocked_users (req: Request, res: Response) {
+export async function list_blocked_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const blockeds_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {blocked: true},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { blocked: true },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(blockeds_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all blocked users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all blocked users",
+            })
+        )
     }
 }
-export async function list_deleted_users (req: Request, res: Response) {
+export async function list_deleted_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const deleteds_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {deleted: true},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { deleted: true },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(deleteds_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all deleted users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all deleted users",
+            })
+        )
     }
 }
-export async function list_verifyed_users (req: Request, res: Response) {
+export async function list_verifyed_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const verifyeds_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {verifyed: true},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { verifyed: true },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(verifyeds_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all verifyed users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all verifyed users",
+            })
+        )
     }
 }
-export async function list_muted_users (req: Request, res: Response) {
+export async function list_muted_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const muteds_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {muted: true},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { muted: true },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(muteds_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all muted users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all muted users",
+            })
+        )
     }
 }
-export async function list_admin_users (req: Request, res: Response) {
+export async function list_admin_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const admins_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {access_level: 1},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { access_level: 1 },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(admins_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all admin users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all admin users",
+            })
+        )
     }
 }
-export async function list_moderator_users (req: Request, res: Response) {
+export async function list_moderator_users(req: Request, res: Response) {
     const { page, pageSize } = req.query
     const offset = (Number(page) - 1) * Number(pageSize)
 
     try {
         const moderators_list = await User.findAndCountAll({
-            attributes: ['username', 'verifyed', 'name', 'last_active_at'],
-            where: {access_level: 2},
+            attributes: ["username", "verifyed", "name", "last_active_at"],
+            where: { access_level: 2 },
             include: [
                 {
                     model: ProfilePicture,
-                    as: 'profile_pictures',
-                    attributes: ['tiny_resolution']
-                }
+                    as: "profile_pictures",
+                    attributes: ["tiny_resolution"],
+                },
             ],
             offset,
             limit: Number(pageSize),
         })
         res.status(200).json(moderators_list)
     } catch (err) {
-        res.status(400).send( new NotFoundError({
-            message: 'Unable to list all moderators users',
-        }))
+        res.status(400).send(
+            new NotFoundError({
+                message: "Unable to list all moderators users",
+            })
+        )
     }
 }
--- src/controllers/admin/admin-user-controller.ts
+++ src/controllers/admin/admin-user-controller.ts
@@ -1,138 +1,178 @@
-import { Request, Response } from 'express'
-import { ValidationError } from "../../errors"
-import User from '../../models/user/user-model.js'
+import { Request, Response } from "express"
+import { InternalServerError, ValidationError } from "../../errors"
+import User from "../../models/user/user-model"
 
-export async function block_user (req: Request, res: Response) {
-    const { user_id }  = req.body
+export async function block_user(req: Request, res: Response) {
+    const { user_id } = req.body
 
     const user = await User.findOne({
-        attributes: ['blocked', 'id'],
-        where: {id: user_id}
+        attributes: ["blocked", "id"],
+        where: { id: user_id },
     })
 
-    if(user.blocked == true){
-        res.status(400).send( new ValidationError({
-            message: 'This user has been blocked previously',
-            action: 'Make sure the user you want to block is unlocked',
-        }))
-    }else {
-        await User.update({ blocked: true }, {
-            where: {id: user.id}
-        })
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
+
+    if (user.blocked == true) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has been blocked previously",
+                action: "Make sure the user you want to block is unlocked",
+            })
+        )
+    } else {
+        await User.update(
+            { blocked: true },
+            {
+                where: { id: user.id },
+            }
+        )
         res.status(200).json({
-            message: 'This user has been blocked successfully'
+            message: "This user has been blocked successfully",
         })
     }
 }
-export async function unlock_user (req: Request, res: Response) {
-    const { user_id }  = req.body
+export async function unlock_user(req: Request, res: Response) {
+    const { user_id } = req.body
 
     const user = await User.findOne({
-        attributes: ['blocked', 'id'],
-        where: {id: user_id}
+        attributes: ["blocked", "id"],
+        where: { id: user_id },
     })
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
 
-    if(user.blocked == false){
-        res.status(400).send( new ValidationError({
-            message: 'This user has not been blocked previusly',
-            action: 'Make sure the user you want to unlock has been blocked previously',
-        }))
-    }else {
-        await User.update({ blocked: false }, {
-            where: {id: user.id}
-        })
+    if (user.blocked == false) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has not been blocked previusly",
+                action: "Make sure the user you want to unlock has been blocked previously",
+            })
+        )
+    } else {
+        await User.update(
+            { blocked: false },
+            {
+                where: { id: user.id },
+            }
+        )
         res.status(200).json({
-            message: 'This user has been unblocked successfully'
-        })
-    }
-}
-
-export async function verify_user (req: Request, res: Response) {
-    const { user_id }  = req.body
-
-    const user = await User.findOne({
-        attributes: ['verifyed', 'id'],
-        where: {id: user_id}
-    })
-
-    if(user.verifyed == true){
-        res.status(400).send( new ValidationError({
-            message: 'This user has been verifyed previously',
-            action: 'Make sure the user you want to verify if you have this seal',
-        }))
-    }else {
-        await User.update({ verifyed: true }, {
-            where: {id: user.id}
-        })
-        res.status(200).json({
-            message: 'This user has been verifyed successfully'
-        })
-    }
-}
-export async function unverify_user (req: Request, res: Response) {
-    const { user_id }  = req.body
-
-    const user = await User.findOne({
-        attributes: ['verifyed', 'id'],
-        where: {id: user_id}
-    })
-
-    if(user.verifyed == false){
-        res.status(400).send( new ValidationError({
-            message: 'This user has not been verifyed previusly',
-            action: 'Check that the user you want to remove the verification has this seal',
-        }))
-    }else {
-        await User.update({ verifyed: false }, {
-            where: {id: user.id}
-        })
-        res.status(200).json({
-            message: 'This user has been unverifyed successfully'
+            message: "This user has been unblocked successfully",
         })
     }
 }
 
-export async function mute_user (req: Request, res: Response) {
-    const { user_id }  = req.body
+export async function verify_user(req: Request, res: Response) {
+    const { user_id } = req.body
 
     const user = await User.findOne({
-        attributes: ['muted', 'id'],
-        where: {id: user_id}
+        attributes: ["verifyed", "id"],
+        where: { id: user_id },
     })
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
 
-    if(user.muted == true){
-        res.status(400).send( new ValidationError({
-            message: 'This user has been muted previously',
-            action: 'Make sure the user you want to mute is unmuted',
-        }))
-    }else {
-        await User.update({ muted: true }, {
-            where: {id: user.id}
-        })
+    if (user.verifyed == true) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has been verifyed previously",
+                action: "Make sure the user you want to verify if you have this seal",
+            })
+        )
+    } else {
+        await User.update(
+            { verifyed: true },
+            {
+                where: { id: user.id },
+            }
+        )
         res.status(200).json({
-            message: 'This user has been muted successfully'
+            message: "This user has been verifyed successfully",
         })
     }
 }
-export async function unmute_user (req: Request, res: Response) {
-    const { user_id }  = req.body
+export async function unverify_user(req: Request, res: Response) {
+    const { user_id } = req.body
 
     const user = await User.findOne({
-        attributes: ['muted', 'id'],
-        where: {id: user_id}
+        attributes: ["verifyed", "id"],
+        where: { id: user_id },
     })
 
-    if(user.muted == false){
-        res.status(400).send( new ValidationError({
-            message: 'This user has not been muted previusly',
-            action: 'Make sure the user you want to unmute has been muted previously',
-        }))
-    }else {
-        await User.update({ muted: false }, {
-            where: {id: user.id}
-        })
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
+
+    if (user.verifyed == false) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has not been verifyed previusly",
+                action: "Check that the user you want to remove the verification has this seal",
+            })
+        )
+    } else {
+        await User.update(
+            { verifyed: false },
+            {
+                where: { id: user.id },
+            }
+        )
         res.status(200).json({
-            message: 'This user has been unmuted successfully'
+            message: "This user has been unverifyed successfully",
+        })
+    }
+}
+
+export async function mute_user(req: Request, res: Response) {
+    const { user_id } = req.body
+
+    const user = await User.findOne({
+        attributes: ["muted", "id"],
+        where: { id: user_id },
+    })
+
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
+
+    if (user.muted == true) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has been muted previously",
+                action: "Make sure the user you want to mute is unmuted",
+            })
+        )
+    } else {
+        await User.update(
+            { muted: true },
+            {
+                where: { id: user.id },
+            }
+        )
+        res.status(200).json({
+            message: "This user has been muted successfully",
+        })
+    }
+}
+export async function unmute_user(req: Request, res: Response) {
+    const { user_id } = req.body
+
+    const user = await User.findOne({
+        attributes: ["muted", "id"],
+        where: { id: user_id },
+    })
+
+    if (!user) throw new InternalServerError({ message: "Can't possible find this user." })
+
+    if (user.muted == false) {
+        res.status(400).send(
+            new ValidationError({
+                message: "This user has not been muted previusly",
+                action: "Make sure the user you want to unmute has been muted previously",
+            })
+        )
+    } else {
+        await User.update(
+            { muted: false },
+            {
+                where: { id: user.id },
+            }
+        )
+        res.status(200).json({
+            message: "This user has been unmuted successfully",
         })
     }
 }
