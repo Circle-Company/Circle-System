be3a9c4c1049ac4e3a37d83b6b9fe7c761eca078
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-16 23:38:37 -0300

    add: error handling + remove number conversor

--- src/controllers/memory/memory-store-controller.ts
+++ src/controllers/memory/memory-store-controller.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
+import { InternalServerError } from "../../errors"
 import { Memory } from "../../services/memory-service"
 
 export async function store_new_memory(req: Request, res: Response) {
     const { user_id, title } = req.body
     const result = await Memory.Store.NewMemory({ user_id, title })
+
     res.status(StatusCodes.ACCEPTED).json(result)
 }
 
 export async function store_new_memory_moment(req: Request, res: Response) {
     const { memory_id, moments_list } = req.body
+
+    if (!req.user_id) throw new InternalServerError({ message: "req.user_id is missing." })
     const result = await Memory.Store.NewMemoryMoment({
-        user_id: Number(req.user_id),
+        user_id: req.user_id,
         memory_id,
         moments_list,
     })
