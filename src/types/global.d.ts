2a6d3f5f0ba1b805c48cb838096e3f0da6fdfdf1
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-16 23:48:37 -0300

    add global declarations to .js models

---
+++ src/types/global.d.ts
@@ -0,0 +1,89 @@
+declare module "models/comments/comment-model.js" {
+    const Comment: any
+    export default Comment
+}
+
+declare module "models/comments/comment_likes-model.js" {
+    const CommentLike: any
+    export default CommentLike
+}
+
+declare module "models/comments/comment_statistics-model.js" {
+    const CommentStatistic: any
+    export default CommentStatistic
+}
+
+declare module "models/moments/moment_interaction-model.js" {
+    const MomentInteraction: any
+    export default MomentInteraction
+}
+
+declare module "models/moments/moment_metadata-model.js" {
+    const MomentMetadata: any
+    export default MomentMetadata
+}
+
+declare module "models/moments/moment_midia-model.js" {
+    const MomentMidia: any
+    export default MomentMidia
+}
+
+declare module "models/moments/moment_statistic-model.js" {
+    const MomentStatistic: any
+    export default MomentStatistic
+}
+
+declare module "models/moments/moment_tag-model.js" {
+    const MomentTag: any
+    export default MomentTag
+}
+
+declare module "models/moments/profile_click-model.js" {
+    const ProfileClick: any
+    export default ProfileClick
+}
+
+declare module "models/moments/share-model.js" {
+    const Share: any
+    export default Share
+}
+
+declare module "models/moments/skip-model.js" {
+    const Skip: any
+    export default Skip
+}
+
+declare module "models/moments/view-model.js" {
+    const View: any
+    export default View
+}
+
+declare module "models/notification/notification_token-model.js" {
+    const NotificationToken: any
+    export default NotificationToken
+}
+
+declare module "models/preferences/preference-model.js" {
+    const Preference: any
+    export default Preference
+}
+
+declare module "models/user/block-model.js" {
+    const Block: any
+    export default Block
+}
+
+declare module "models/user/contact-model.js" {
+    const Contact: any
+    export default Contact
+}
+
+declare module "models/user/metadata-model.js" {
+    const Metadata: any
+    export default Metadata
+}
+
+declare module "models/user/report-model.js" {
+    const Report: any
+    export default Report
+}
