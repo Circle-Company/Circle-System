29e82a8c381ae062c5b26eee8377c3e9b7b2f0ec
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-01-16 23:38:04 -0300

    fix: change number to bigint

--- src/controllers/memory/memory-find-controller.ts
+++ src/controllers/memory/memory-find-controller.ts
@@ -7,8 +7,10 @@ export async function find_memory_moments(req: Request, res: Response) {
     const { memory_id } = req.body
     const page = parseInt(req.query.page as string, 10) || 1
     const pageSize = parseInt(req.query.pageSize as string, 10) || 10
+
+    if (!req.user_id) throw new InternalServerError({ message: "req.user_id is missing." })
     const result = await Memory.Find.Moments({
-        user_id: Number(req.user_id),
+        user_id: BigInt(req.user_id),
         memory_id,
         page,
         pageSize,
