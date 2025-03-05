34541b04bd93899543a0f8cc4285cfee1969c480
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-04 21:33:43 -0300

    fix: firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,18 +1,19 @@
-import config from "config"
+import * as dotenv from "dotenv"
 import admin from "firebase-admin"
+dotenv.config()
 
 const credential: any = {
     type: "service_account",
-    project_id: `${config.FIREBASE_PROJECT_ID}`,
-    private_key_id: `${config.FIREBASE_PRIVATE_KEY_ID}`,
-    private_key: `${config.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")}`, // Corrige quebras de linha
-    client_email: `${config.FIREBASE_CLIENT_EMAIL}`,
-    client_id: `${config.FIREBASE_CLIENT_ID}`,
-    auth_uri: `${config.FIREBASE_AUTH_URI}`,
-    token_uri: `${config.FIREBASE_TOKEN_URI}`,
-    auth_provider_x509_cert_url: `${config.FIREBASE_AUTH_PROVIDER_CERT_URL}`,
-    client_x509_cert_url: `${config.FIREBASE_CLIENT_CERT_URL}`,
-    universe_domain: `${config.FIREBASE_UNIVERSE_DOMAIN}`,
+    project_id: process.env.FIREBASE_PROJECT_ID || "",
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID || "",
+    private_key: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL || "",
+    client_id: process.env.FIREBASE_CLIENT_ID || "",
+    auth_uri: process.env.FIREBASE_AUTH_URI || "",
+    token_uri: process.env.FIREBASE_TOKEN_URI || "",
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL || "",
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL || "",
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN || "",
 }
 
 admin.initializeApp({
