55fe564ccbb7a1a907676724bc724db02a31e09c
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-07 23:25:10 -0300

    refactor: remove unused files and folders

--- src/swipe-engine/src/connection/routes/route-swipe_engine.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { SwipeEngine } from "../../.."
-
-const swipeEngineRouter = Router()
-swipeEngineRouter.post("/moments/get/feed", SwipeEngine.getMoments)
-swipeEngineRouter.post("/moments/store/interaction", SwipeEngine.storeMomentInteraction)
-
-export default swipeEngineRouter
--- src/swipe-engine/src/math/sigmoid.ts
+++
@@ -1,3 +0,0 @@
-export default function sigmoid(z: number) {
-    return 1 / (1 + Math.exp(-z))
-}
