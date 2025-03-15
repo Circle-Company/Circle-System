b33a181aa3df79fa3c172235287cadba30391fe5
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-15 17:38:54 -0300

    fix: if tollowers is 0 return 0

--- src/search_engine/src/modules/related_candidates/calcule_score.ts
+++ src/search_engine/src/modules/related_candidates/calcule_score.ts
@@ -38,7 +38,9 @@ export function calcule_score({ candidates_with_informations }: SortCandidatesPr
             you_follow: candidate.you_follow,
             profile_picture: candidate.profile_picture,
             statistic: {
-                total_followers_num: candidate.statistic.total_followers_num,
+                total_followers_num: candidate.statistic.total_followers_num
+                    ? candidate.statistic.total_followers_num
+                    : 0,
             },
             score: totalScore,
         }
