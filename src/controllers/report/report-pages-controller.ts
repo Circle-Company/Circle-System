ad5dfbd70d2ecff9d7fe35ad75170dcfc4a3bd86
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-03-26 22:13:04 -0300

    refactor: change controller name

--- src/controllers/report/report-pages-controller.ts
+++ src/controllers/report/report-pages-controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import path from "path"
 
-export async function reportAccount(req: Request, res: Response) {
+export async function reportAccountPage(req: Request, res: Response) {
     const page = path.join(__dirname, "../../../public/pages/report/index.html")
     res.status(200).sendFile(page)
 }
