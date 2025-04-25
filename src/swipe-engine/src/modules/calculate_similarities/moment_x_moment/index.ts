368a23279ac039bbc2457eeb87f114a507a1f5e1
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-04-25 10:18:17 -0300

    refactor: remove obsolete modules and algorithms from swipe engine, streamlining codebase and improving maintainability

--- src/swipe-engine/src/modules/calculate_similarities/moment_x_moment/index.ts
+++
@@ -1,5 +0,0 @@
-import find_posts from "./find_posts"
-export default async function moment_x_moment() {
-    return await find_posts()
-    //return  mapSimilaritiesToMoments(similarityMatrix, similrityIndex)
-}
