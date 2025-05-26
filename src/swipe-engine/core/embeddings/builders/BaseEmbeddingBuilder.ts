352eebbd9c8c2c91933669866f1d5fce98583835
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-05-26 14:39:22 -0300

    refactor: remover a interface IEmbeddingBuilder do projeto, que não está mais em uso

--- src/swipe-engine/core/embeddings/builders/BaseEmbeddingBuilder.ts
+++
@@ -1,7 +0,0 @@
-import { EmbeddingVector } from "../../types"
-
-export interface IEmbeddingBuilder<T> {
-    build(data: T): Promise<EmbeddingVector>
-    update(currentEmbedding: EmbeddingVector, newData: Partial<T>): Promise<EmbeddingVector>
-    validate(data: T): boolean
-} 
