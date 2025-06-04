b0ce859dbdbde15ea5a4133d10fe018f1850c9b4
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-06-04 11:37:36 -0300

    feat: adicionar rotas para denúncia de conta e integrar controlador de ações de denúncia

--- src/routes/report-router.ts
+++ src/routes/report-router.ts
@@ -1,9 +1,14 @@
+import { RP } from "../config/routes_prefix"
 import { ReportController } from "@controllers/report"
 import { Router } from "express"
-import { RP } from "../config/routes_prefix"
+import { reportAccount } from "@controllers/report/report-actions-controller"
 
 const PAGE_PREFIX = "/page"
 
 export const router = Router()
 
+// Página de denúncia de conta
 router.get(PAGE_PREFIX + RP.ACCOUNT, ReportController.ReportAccount)
+
+// API de denúncia de conta (integração com o formulário)
+router.post(RP.API_VERISON + RP.REPORT + "/account", reportAccount)
