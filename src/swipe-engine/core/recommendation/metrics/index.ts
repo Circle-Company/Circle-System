918d8bf667bfd5b0ef023b8d31297c75e26b71f6
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-06-15 14:47:16 -0300

    feat: criar índice de métricas para exportação de funções de cálculo, facilitando as importações de afinidade, diversidade, engajamento, novidade, qualidade e relevância temporal

---
+++ src/swipe-engine/core/recommendation/metrics/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Metrics Index
+ * 
+ * Exporta todas as funções de cálculo de métricas para facilitar as importações.
+ */
+
+export { calculateAffinityScore } from './AffinityMetrics'
+export { calculateDiversityScore } from './DiversityMetrics'
+export { calculateEngagementScore } from './EngagementMetrics'
+export { calculateNoveltyScore } from './NoveltyMetrics'
+export { calculateQualityScore } from './QualityMetrics'
+export { calculateTemporalScore } from './TemporalMetrics' 
