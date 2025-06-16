d3f9b9f8ad35415428fa28d781b019146302f3a1
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-06-16 03:00:02 -0300

    feat: criar arquivo de índice para o controlador de métricas da Swipe Engine, exportando funções relacionadas a métricas de sistema, clusters, embeddings e interações do usuário, facilitando a organização e acesso às métricas do sistema de recomendação

---
+++ src/controllers/swipe-engine/index.ts
@@ -0,0 +1,11 @@
+export {
+    getSystemMetrics,
+    getClusterMetrics,
+    getEmbeddingMetrics,
+    getClusterRankingMetrics,
+    getUserInteractionMetrics,
+    getPerformanceMetrics,
+    getRecommendationQualityMetrics,
+    getTemporalTrends,
+    getComprehensiveReport
+} from './swipe-engine-metrics-controller' 
