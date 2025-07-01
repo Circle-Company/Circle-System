182fbe21915d803a8dde0d9da9ce0b5ecc6b7b62
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-07-01 14:13:07 -0300

    refactor: remover comentário @ts-ignore no find_user_block para melhorar a clareza do código

--- src/search_engine/src/functions/set_interactions/find_user_block.ts
+++ src/search_engine/src/functions/set_interactions/find_user_block.ts
@@ -9,7 +9,6 @@ export async function FinduserBlock({
     user_id,
     blocked_user_id,
 }: FindUserBlock): Promise<boolean | null> {
-    // @ts-ignore
     const user_blocked = await Block.findOne({
         attributes: ["blocked_user_id", "user_id"],
         where: { blocked_user_id, user_id },
